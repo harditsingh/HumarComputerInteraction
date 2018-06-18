92c643eb06360d2f5ef57544ecb3a672e2279b1d
BDitta <bekesi.ditta@gmail.com>
2018-06-18 13:48:37 +0200

    fixed the discussion CSS/JS thingy

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ $(function () {
     offset: '0%'
   });
 });
+
+
+/* Discussion JS */
+
+function main() {
+  document.getElementById("read-more-button").addEventListener("click", readMore);
+}
+
+window.addEventListener("load", main);
+
+function readMore() {
+  var x = document.getElementById("read-more-button");
+  if (x.className === "read-more-button") {
+      x.className = "read-less-button";
+      document.getElementById("hidden-discussion").style.display = "block";
+      x.innerHTML = "read less";
+  } else if (x.className === "read-less-button") {
+      /*x.className = "read-more-button";
+      document.getElementById("hidden-discussion").style.display = "none";
+      x.innerHTML = "read more";*/
+  }
+}
