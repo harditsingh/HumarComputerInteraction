3075f454f64ee1850557d04f1522d2e7f75276e9
BDitta <bekesi.ditta@gmail.com>
2018-06-22 23:20:15 +0200

    added headers to Contact and Discussion

--- js/script.js
+++ js/script.js
@@ -27,11 +27,11 @@ function readMore() {
   var x = document.getElementById("read-more-button");
   if (x.className === "read-more-button") {
       x.className = "read-less-button";
-      document.getElementById("hidden-discussion").style.display = "block";
+      document.getElementsByClassName("hidden-discussion").style.display = "block";
       x.innerHTML = "read less";
   } else if (x.className === "read-less-button") {
       /*x.className = "read-more-button";
-      document.getElementById("hidden-discussion").style.display = "none";
+      document.getElementsByClassName("hidden-discussion").style.display = "none";
       x.innerHTML = "read more";*/
   }
 }
