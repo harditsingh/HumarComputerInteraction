b26c7e5a946c7e73cd1d3c60f3f292f7a7b55870
polinab <p.boneva17@gmail.com>
2018-06-23 17:49:04 +0200

    fixed readmore button and added comments on JS main, designer and script

--- js/main.js
+++ js/main.js
@@ -1,35 +1,61 @@
+/**
+ * Loading and main page functionality
+ */
 function main() {
+    /** Open menu on mobile when clicked on "hamburger" icon
+     * Id menu-toggle: hamburger icon
+     * Id popup: menu 
+    */
     document.getElementById("menu-toggle").addEventListener("click", function () {
         const menuPopup = document.getElementById("popup");
 
+        // Open menu if it's closed and vice versa
         if (menuPopup.classList.contains("active")) {
             menuPopup.classList.remove("active");
         } else {
             menuPopup.classList.add("active")
         }
     });
-
+    
+    /**
+     * Close newsletter bar if (X) button is clicked
+     */
     if (document.getElementById("newsletter-bar-close") != null) {
         document.getElementById("newsletter-bar-close").addEventListener('click', function () {
             document.getElementById("newsletter-bar").style.display = "none";
         })
     }
-
+    
+    /**
+     * Progress bar on loading page
+     */
     if(document.getElementById("progress-bar") != null) {
         document.getElementById("progress-bar").style.width = "100%";
     }
-
+    
+    /**
+     * Loading page greeting message
+     */
     if(document.getElementById("loading-message") != null) {
         let element = document.getElementById("loading-message");
         element.style.transform = "translateY(0px)";
         element.style.opacity = "1";
     }
 
+    /**
+     * Fade out loading page after const time
+     */
     setTimeout(hideLoadFrame, 4000);
+    
     addAccordionListeners();
     addCartListeners();
 }
 
+/**
+ * Define functionality of FAQ vertically aligned list section
+ * @param acc: vertically aligned list of frequently asked questions
+ * - On click: open panel with answer
+ */
 function addAccordionListeners() {
     var acc = document.getElementsByClassName("accordion");
     var i;
@@ -47,6 +73,10 @@ function addAccordionListeners() {
     }
 }
 
+/**
+ * Add event listener to all 'Add to cart' buttons
+ * On click: increase number of amount of items in cart
+ */
 function addCartListeners() {
     let listOfCartButtons = document.getElementsByClassName("cart");
     console.log(listOfCartButtons);
@@ -55,6 +85,9 @@ function addCartListeners() {
     }
 }
 
+/**
+ * Increase amount of items in cart
+ */
 function addToCart() {
     let cartCounts = document.getElementsByClassName("cart-count");
     for (let i = 0; i < cartCounts.length; i++) {
@@ -63,8 +96,14 @@ function addToCart() {
     }
 }
 
+/**
+ * Hide the loading bar of loading page
+ */
 function hideLoadFrame() {
     document.getElementById("loading-container").style.display = "none";
 }
 
+/**
+ * Start up main on load
+ */
 window.addEventListener("load", main)
--- js/script.js
+++ js/script.js
@@ -16,22 +16,32 @@ $(function () {
 
 
 /* Discussion JS */
-
-function main() {
-  document.getElementById("read-more-button").addEventListener("click", readMore);
+/**
+ * Open/Close hidden part of the discussion
+ */
+function alterDiscussion() {
+  if (document.getElementById("read-more-button").className === "read-more-button") {
+    readMore();
+  }
+  else if (document.getElementById("read-more-button").className === "read-less-button") {
+    readLess();
+  }
 }
 
-window.addEventListener("load", main);
-
+/**
+ * Open full discussion board when clicked on Read More
+ */
 function readMore() {
-  var x = document.getElementById("read-more-button");
-  if (x.className === "read-more-button") {
-      x.className = "read-less-button";
-      document.getElementById("hidden-discussion").style.display = "block";
-      x.innerHTML = "read less";
-  } else if (x.className === "read-less-button") {
-      /*x.className = "read-more-button";
-      document.getElementById("hidden-discussion").style.display = "none";
-      x.innerHTML = "read more";*/
-  }
+  document.getElementById("read-more-button").className = "read-less-button";
+  document.getElementById("hidden-discussion").style.display = "block";
+  document.getElementById("read-more-button").innerHTML = "read less";
+}
+
+/**
+ * Close most of the discussion board when clicked on Read Less
+ */
+function readLess() {
+  document.getElementById("read-more-button").className = "read-more-button";
+  document.getElementById("hidden-discussion").style.display = "none";
+  document.getElementById("read-more-button").innerHTML = "read more";
 }
