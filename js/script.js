0f753b8990ba6389dcef4704dab68bb7e581fc18
BDitta <bekesi.ditta@gmail.com>
2018-06-23 14:53:01 +0200

    fixed the search and JS on the Discussion page

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(function () {
 });
 
 
-/* Discussion JS */
+/* READ MORE BUTTON ON THE DISCUSSION PAGE */
 
 function main() {
   document.getElementById("read-more-button").addEventListener("click", readMore);
@@ -24,14 +24,23 @@ function main() {
 window.addEventListener("load", main);
 
 function readMore() {
-  var x = document.getElementById("read-more-button");
+  document.getElementById("read-more-button").style.display = "none";
+  var x = document.getElementsByClassName("hidden-discussion")
+  x[0].style.display = "block";
+  x[1].style.display = "block";
+  /* the original try with a ReadMore / ReadLess combination */
+  /*var x = document.getElementById("read-more-button");
   if (x.className === "read-more-button") {
       x.className = "read-less-button";
-      document.getElementsByClassName("hidden-discussion").style.display = "block";
+      var y = document.getElementsByClassName("hidden-discussion")
+      y[0].style.display = "block";
+      y[1].style.display = "block";
       x.innerHTML = "read less";
   } else if (x.className === "read-less-button") {
-      /*x.className = "read-more-button";
-      document.getElementsByClassName("hidden-discussion").style.display = "none";
-      x.innerHTML = "read more";*/
-  }
+      x.className = "read-more-button";
+      var y = document.getElementsByClassName("hidden-discussion")
+      y[0].style.display = "none";
+      y[1].style.display = "none";
+      x.innerHTML = "read more";
+  }*/
 }
