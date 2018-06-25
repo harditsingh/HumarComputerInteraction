e462ee9e786cbf746916c9b7cc99f35fa294490f
BDitta <bekesi.ditta@gmail.com>
2018-06-25 08:37:58 +0200

    fixed the validation for the contact & discussion pages

--- js/script.js
+++ js/script.js
@@ -56,30 +56,44 @@ $(function () {
 
 /* Discussion JS */
 
-function main() {
-  document.getElementById("read-more-button").addEventListener("click", readMore);
+/**
+ * Open/Close hidden part of the discussion
+ */
+function alterDiscussion() {
+  if (document.getElementById("read-more-button").className === "read-more-button") {
+    readMore();
+  }
+  else if (document.getElementById("read-more-button").className === "read-less-button") {
+    readLess();
+  }
 }
 
-window.addEventListener("load", main);
-
+/**
+ * Open full discussion board when clicked on Read More
+ */
 function readMore() {
-  let readMoreButton = document.getElementById("read-more-button");
+  document.getElementById("read-more-button").className = "read-less-button";
 
-  if (readMoreButton.classList.contains("read-less")) {
-    readMoreButton.classList.remove("read-less");
-
-    let hiddenDisscussions = document.getElementsByClassName("hidden-discussion")
-    for (let i = 0; i < hiddenDisscussions.length; i++) {
-      hiddenDisscussions[i].style.display = "none";
-    }
-    readMoreButton.innerHTML = "Read More";
-  } else {
-    readMoreButton.classList.add("read-less");
-
-    let hiddenDisscussions = document.getElementsByClassName("hidden-discussion")
-    for (let i = 0; i < hiddenDisscussions.length; i++) {
-      hiddenDisscussions[i].style.display = "block";
-    }
-    readMoreButton.innerHTML = "Read Less";
+  var reviews = document.getElementsByClassName("hidden-discussion");
+  for(var i = 0; i < reviews.length; i++)
+  {
+    document.getElementsByClassName("hidden-discussion")[i].style.display = "block";
   }
+
+  document.getElementById("read-more-button").innerHTML = "read less";
+}
+
+/**
+ * Close most of the discussion board when clicked on Read Less
+ */
+function readLess() {
+  document.getElementById("read-more-button").className = "read-more-button";
+
+  var reviews = document.getElementsByClassName("hidden-discussion");
+  for(var i = 0; i < reviews.length; i++)
+  {
+    document.getElementsByClassName("hidden-discussion")[i].style.display = "none";
+  }
+
+  document.getElementById("read-more-button").innerHTML = "read more";
 }
